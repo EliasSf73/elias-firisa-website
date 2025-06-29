327f3493b3585d6f339a963c3b192f7ea75fcd82
Elais Firisa <eliassfirisa@gmail.com>
2025-06-29 19:21:53 +0900

    Simplify GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const nextConfig = {
   },
   basePath: '/elias_firisa_webiste',
   trailingSlash: true,
-  assetPrefix: 'https://eliasfirisa.github.io/elias_firisa_webiste/',
 };
 
 module.exports = nextConfig
