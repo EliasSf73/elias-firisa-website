6be5b8abf48877c327e2a0b8505fdef162b675b0
Elais Firisa <eliassfirisa@gmail.com>
2025-06-29 19:35:01 +0900

    Update to modern Next.js export method

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
   basePath: '/elias_firisa_webiste',
   trailingSlash: true,
   assetPrefix: '/elias_firisa_webiste/',
+  experimental: {
+    optimizeCss: true,
+    optimizeFonts: true,
+  },
 };
 
 module.exports = nextConfig
