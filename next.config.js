e0e6b21179687ece91ab511071357b2884127654
Elais Firisa <eliassfirisa@gmail.com>
2025-06-29 19:06:41 +0900

    Update GitHub Pages configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-}
+  images: {
+    unoptimized: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://eliasfirisa.github.io/elias_firisa_webiste/:path*',
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig
