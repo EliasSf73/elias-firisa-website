98105309b89796f85a18de52b2d56962f99862e9
Elais Firisa <eliassfirisa@gmail.com>
2025-08-06 19:32:05 +0900

    updated home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,16 +16,24 @@ export default function Home() {
             I am a 4th-year Bachelor student at <Link href="https://www.kaist.ac.kr/en/" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:underline">KAIST</Link>,
             majoring in <Link href="https://bcs.kaist.ac.kr/" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:underline">Brain and Cognitive Science (BCS) </Link>
             with a minor in Artificial Intelligence (AI).
-
           </p>
           <p>
             My interests lie at the intersection of artificial intelligence and computational neuroscience,
             where I explore how the brain processes information and how we can apply these principles to build more intelligent systems.
           </p>
           <p>
-            This website serves as a platform to showcase my projects, share valuable resources,
-            and publish mathematically rigorous blogs on topics within my field of study.
+            This website serves as a platform to showcase my projects, share valuable resources, and publish mathematically rigorous blogs on topics within my field of study.
           </p>
+          <div className="relative p-6 my-6 bg-gray-50 rounded-2xl border border-gray-200 shadow-sm hover:shadow-md transition-shadow duration-300">
+            <div className="absolute -top-3 -left-3 w-6 h-6 bg-blue-100 rounded-full"></div>
+            <div className="absolute -bottom-3 -right-3 w-6 h-6 bg-blue-100 rounded-full"></div>
+            <div className="absolute -top-3 -right-3 w-6 h-6 bg-blue-100 rounded-full"></div>
+            <div className="absolute -bottom-3 -left-3 w-6 h-6 bg-blue-100 rounded-full"></div>
+            
+            <p className="relative z-10">
+              Outside school stuff, I'm a wannabe footballer and die-hard fan<Link href="https://en.wikipedia.org/wiki/Big_Five_(association_football)" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:underline"> ⚽🦯</Link> (never miss a match), and I'm also a huge Christopher Nolan enthusiast—<Link href="https://en.wikipedia.org/wiki/Interstellar_(film)" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:underline">Interstellar</Link> and <Link href="https://en.wikipedia.org/wiki/Inception" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:underline">Inception</Link> rank at the very top for me.
+            </p>
+          </div>
         </div>
       </section>
     </div>
